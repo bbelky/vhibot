797b45e5ca418833aa568eb8e6eea4c5d084cd5a
Vladimir Porokhov <vporokhov@virtuozzo.com>
2024-03-03 15:34:25 +0200

    add betterstack

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import dotenv
 import requests
+import logging
 from langchain_openai import OpenAIEmbeddings 
 from langchain_community.vectorstores import FAISS 
 from langchain_community.document_loaders import PyPDFLoader
@@ -12,6 +13,7 @@ from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from telegram.ext import CommandHandler, MessageHandler, filters, Application
+from logtail import LogtailHandler
 
 #Load local environment variables
 dotenv.load_dotenv()
@@ -22,6 +24,7 @@ embeddings_name = os.getenv("EMB_MODEL_NAME")
 url1 = os.getenv("FILE1_PATH")
 url2 = os.getenv("FILE2_PATH")
 load_type = os.getenv("LOAD_TYPE")
+betterstack_token = os.getenv("BETTERSTACK_TOKEN")
 
 #Load documents
 if load_type == "PDF":
@@ -68,14 +71,30 @@ prompt = PromptTemplate(
     input_variables=["query"], 
     template=template,
 )
+
+#BetterStack Logger
+handler = LogtailHandler(source_token=betterstack_token)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.handlers = []
+logger.addHandler(handler)
+
 print("READY")
 
 async def start_command(update, context):
   # Implement the start response
+    user = update.message.from_user
+    logger.info('start', extra={
+        'user': user['username'],
+    }) 
     await update.message.reply_text('Welcome to the AI-powered Virtuozzo Hybrid Infrastructure Support Bot!\nExample:\nHow to configure Kubernetes storage class?')
 
 async def product_command(update, context):
   # Implement the wiki response
+    user = update.message.from_user
+    logger.info('product', extra={
+        'user': user['username'],
+    }) 
     await update.message.reply_html(
         "<a href='https://www.youtube.com/watch?v=n5jH82nOyF8&list=PL86FC0XuGZPJ-EDDSwT-MoSyYHTG5cjeb'>Product Demo</a>\n"
         "<a href='https://www.virtuozzo.com/hybrid-infrastructure/'>Product Page</a>\n",
@@ -83,6 +102,10 @@ async def product_command(update, context):
 
 async def wiki_command(update, context):
   # Implement the wiki response
+    user = update.message.from_user
+    logger.info('wiki', extra={
+        'user': user['username'],
+    }) 
     await update.message.reply_html(
         "The list of how-to, integration examples and troubleshooting guides\n"
         "<a href='https://virtuozzo.atlassian.net/wiki/spaces/WIKI/pages/2681176121/Virtuozzo+Hybrid+Infrastructure/'>Wiki Pages</a>\n"
@@ -91,6 +114,10 @@ async def wiki_command(update, context):
 
 async def docs_command(update, context):
   # Implement the wiki response
+    user = update.message.from_user
+    logger.info('docs', extra={
+        'user': user['username'],
+    }) 
     await update.message.reply_html(
         "<a href='https://docs.virtuozzo.com/virtuozzo_hybrid_infrastructure_6_0_admins_guide/index.html'>Administrator's Guide</a>\n"
         "<a href='https://docs.virtuozzo.com/virtuozzo_hybrid_infrastructure_6_0_self_service_guide/index.html'>Self-service Guide</a>\n"
@@ -103,7 +130,11 @@ async def docs_command(update, context):
 
 async def support_command(update, context):
   # Implement the wiki response
-     await update.message.reply_html(
+    user = update.message.from_user
+    logger.info('support', extra={
+        'user': user['username'],
+    }) 
+    await update.message.reply_html(
         "<a href='https://virtuozzo.zendesk.com/auth/v2/login/signin'>Create Ticket</a>\n"
         "<a href='https://support.virtuozzo.com/hc/en-us'>Support Portal</a>\n"
         "<a href='https://www.virtuozzo.com/all-supported-products/severity-level-definitions/'>Priority Definitions</a>",
@@ -120,13 +151,25 @@ async def handle_message(update, context):
       prompt.format(query=message)
     )
   print(cb)
+
+  #betterstack logs
+  #logger.error('Something bad happened.')
+  logger.info('question', extra={
+    'user': user['username'],
+    'prompt': message,
+    'answer': answer.get('result'),
+    'promptTokens': cb.prompt_tokens,
+    'completionTokens': cb.completion_tokens,
+    'totalCost': cb.total_cost
+  })
+
   answer_formatted = answer.get('result').replace("<", "&lt;").replace(">", "&gt;")          
   #await update.message.reply_text("Question:\n" + message + "\n" + "Answer:\n" + answer.get('result'))
   await update.message.reply_html(answer_formatted)
 
 #Polling Telegram bot
 def main() -> None:
-
+    
     application = Application.builder().token(bot_token).build()
     application.add_handler(CommandHandler('start', start_command))
     application.add_handler(CommandHandler('product', product_command))
